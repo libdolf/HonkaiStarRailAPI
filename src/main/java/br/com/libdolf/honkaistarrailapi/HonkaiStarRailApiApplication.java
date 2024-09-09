2ba63da15db894331624fdebd0f9f992cdb761bd
Lindolfo Moizinho <lindolfomoizinho@gmail.com>
2024-09-09 18:09:22 -0300

    Initial commit

---
+++ src/main/java/br/com/libdolf/honkaistarrailapi/HonkaiStarRailApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.libdolf.honkaistarrailapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HonkaiStarRailApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HonkaiStarRailApiApplication.class, args);
+	}
+
+}
